8c6ffccad9eae850ccfb2c02214cafbea2de59aa
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-02-25 18:26:12 +0700

    add lombok and add logging when access login page (to see if lombok is set up successfully or not)

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,16 +1,19 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping()
     public String getLoginPage(@ModelAttribute("flashAttribute") Object signupSuccess,Model model){
+        log.info("Open login page: /login");
 
         return "login";
     }
